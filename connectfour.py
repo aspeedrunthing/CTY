e6877ec26749226bb39d6e5432365edf57894db0
aspeedrunthing <aspeedrunthing@gmail.com>
2024-06-26 10:53:59 -0400

    Add files via upload

--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,4 @@
-import math
+import math 
 def printboard():
     i=6
     o=1
@@ -13,7 +13,7 @@ player = 2
 changeplayer = 1
 win = 0
 printboard()
-while x != 8:
+while win == 0:
     if changeplayer == 1:
         if player == 1:
             player +=1
@@ -23,6 +23,7 @@ while x != 8:
     x = (input("choose your spot 1-7: "))
     if not x.isdigit():
         printboard()
+        changeplayer = 0
         continue
     if int(math.ceil(float(x)))>8 or int(math.ceil(float(x)))<1:
         print("no")
@@ -40,3 +41,27 @@ while x != 8:
             print("no")
             changeplayer = 0
     printboard()
+    for i in range(0, 6):
+        for t in range(0, 4):
+            if board[i][t] == board[i][t+1] == board[i][t+2] == board[i][t+3] !=0:
+                win += 1
+                print("Player ", board[i][t+3], "has won")
+                break
+    for i in range(0, 3):
+        for t in range(0, 6):
+            if board[i][t] == board[i+1][t] == board[i+2][t] == board[i+3][t] !=0:
+                win += 1
+                print("Player ", board[i][t], "has won")
+                break
+    for i in range(0, 3):
+        for t in range(0, 4):
+            if board[i][t] == board[i+1][t+1] == board[i+2][t+2] == board[i+3][t+3] !=0:
+                win += 1
+                print("Player ", board[i][t], "has won")
+                break
+    for i in range(0, 3):
+        for t in range(0, 4):
+            if board[i][t+3] == board[i+1][t+2] == board[i+2][t+1] == board[i+3][t] !=0:
+                win += 1
+                print("Player ", board[i][t+3], "has won")
+                break
