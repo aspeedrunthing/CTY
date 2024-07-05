9800703757a1ad81b374a4b2db840b61e3cdcd8c
aspeedrunthing <aspeedrunthing@gmail.com>
2024-07-05 14:54:22 -0400

    Add files via upload

--- games.py
+++ games.py
@@ -3,12 +3,13 @@ print("1: Connect Four")
 print("2: Make Your Own Choose Your Own Adventure Game")
 print("3: Minesweeper")
 print("4: Tic Tac Toe")
+print("5: Puzzle Game")
 game = 0
-while game != 1 and game != 2 and game != 3 and game != 4:
+while game != 1 and game != 2 and game != 3 and game != 4 and game != 5:
     game = (input(""))
     if game.isdigit():
         game = int(game)
-    if game != 1 and game != 2 and game != 3 and game != 4:
+    if game != 1 and game != 2 and game != 3 and game != 4 and game != 5:
         print("Choose a valid option")
 if game == 1:
     import connectfour
@@ -18,3 +19,5 @@ if game == 3:
     import minesweeper
 if game == 4:
     import tictactoe
+if game == 5:
+    import puzzle
